cf4ee278a88e8516e2b073d85cb108a6df9168b2
Mahd Raihan <l39583066@student.ua92.ac.uk>
2025-02-01 14:42:50 +0000

    NavBar&Footers v0.1.0
    Added NavBar
    Added Footer

---
+++ assets/js/utils.js
@@ -0,0 +1,47 @@
+
+// Setting Active Page Local Function
+
+document.addEventListener('DOMContentLoaded', function () {
+    const currentPath = window.location.pathname;
+    const navItems = {
+        '/assets/pages/plans.html': 'Plans',
+        '/assets/pages/products.html': 'Products',
+        '/assets/pages/services.html': 'Services',
+        '/assets/pages/about-us.html': 'AboutUs',
+        '/assets/pages/contact-us.html': 'ContactUs'
+    };
+
+    if (navItems[currentPath]) {
+        const navElement = document.getElementById(navItems[currentPath]);
+        if (navElement) {
+            navElement.classList.add('active');
+        }
+    }
+});
+
+// Dark Mode Toggle Function
+
+const toggleButton = document.getElementById('theme-toggle');
+const DRoidBody = document.body;
+
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    DRoidBody.classList.add('dark-mode');
+}
+toggleButton.addEventListener('click', () => {
+    DRoidBody.classList.toggle('dark-mode');
+
+    if (DRoidBody.classList.contains('dark-mode')) {
+        localStorage.setItem('dark-mode', 'enabled');
+    } else {
+        localStorage.removeItem('dark-mode');
+    }
+});
+
+// Dark Mode System Auto Function
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+    DRoidBody.classList.add('dark-mode');
+}
+
